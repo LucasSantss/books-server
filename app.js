4a83e12119370ad2baf032152dcf54c1d5beff7d
LucasSantss <lucas.santosrod@hotmail.com>
2025-03-13 16:09:26 -0300

    criando post para o json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const rotaLivro = require("./rotas/livro")
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.use("/livros", rotaLivro)
 
--- controladores/livros.js
+++ controladores/livros.js
@@ -1,12 +1,40 @@
+const { getTodosLivros, getLivroId, insereLivro } = require("../servicos/livro")
+
 function getLivros(req, res) {
     try {
-        res.send("A rota de livros está funcionando!")
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
-    res.send("Olá, mundo!")
 }
+
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivroId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body
+        insereLivro(livroNovo)
+        res.status(201)
+        res.send("Livro inserido com sucesso!")
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro
 }
