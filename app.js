889a19cb0a15483f7cbba36f118d12f2241c6971
LucasSantss <lucas.santosrod@hotmail.com>
2025-03-15 16:27:26 -0300

    criado post e delete nos favoritos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const rotaFavorito = require("./rotas/favorito")
 const rotaLivro = require("./rotas/livro")
 const express = require('express')
 const cors = require('cors')
@@ -7,6 +8,7 @@ app.use(express.json())
 app.use(cors({ origin: '*' })) //todas as origens podem acessar a API
 
 app.use("/livros", rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
 const port = 8000 //npx nodemon app
 
